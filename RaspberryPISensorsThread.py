7cdc5c32bb3566a4577921518bc41f669047b9c1
Nikita-neuron <077anderwater770@gmail.com>
2021-02-09 17:21:38 +0300

    ПАШЛИТЕ

---
+++ RaspberryPISensorsThread.py
@@ -0,0 +1,95 @@
+import threading
+import queue
+import socket
+
+from socketsMessagesProtocol import MessagesProtocol
+
+class RaspberryPISensorsThread(threading.Thread):
+  def __init__(self, raspberry):
+    super().__init__()
+
+    self._stopped = False
+    self.raspberry = raspberry
+
+    self.ultrasonic_data = queue.Queue(2)
+
+    self.system_data = queue.Queue(2)
+
+    self.messagesProtocol = MessagesProtocol(self.raspberry)
+
+  def run(self):
+    self.print("Connection successful")
+    while not self._stopped:
+      try:
+        message = self.messagesProtocol.receive_message(16)
+
+        if message != "OK":
+          break
+      except:
+        break
+
+      self.messagesProtocol.send_message("OK")
+      
+      try:
+        ultrasonic_data = self.messagesProtocol.receive_message(32)
+        # print("a: " + str(ultrasonic_data))
+
+        if ultrasonic_data == "No connection":
+          break
+        if ultrasonic_data != "No sensors data":
+          self.add_ultrasonic_data(ultrasonic_data)
+      except:
+        break
+      
+      try:
+        sys_data = self.messagesProtocol.receive_message(4096)
+
+        if sys_data == "No connection":
+          break
+        if sys_data != "No system data":
+          self.add_system_data(sys_data)
+      except:
+        break
+      
+    self.print("disconnect Raspberry PI SENSORS")
+    self.stop()
+    self.raspberry.close()
+
+  def add_ultrasonic_data(self, ultrasonic_data):
+    # добавдение показаний с датчиков в очередь
+    try:
+      print("t: " + str(ultrasonic_data))
+      self.ultrasonic_data.put(ultrasonic_data, block=False)
+    except queue.Full:
+      pass
+
+  def get_ultrasonic_data(self):
+    # получение показаний датчиков из очереди
+    ultrasonic_data = None
+    try:
+      ultrasonic_data = self.ultrasonic_data.get_nowait()
+    except queue.Empty:
+      pass
+
+    return ultrasonic_data
+
+  def add_system_data(self, system_data):
+    # добавление системных данных в очередь
+    try:
+      self.system_data.put(system_data)
+    except queue.Full:
+      pass
+
+  def get_sys_data(self):
+    # получение системных данных из очереди
+    try:
+      return self.system_data.get_nowait()
+    except queue.Empty:
+      return None
+
+  def print(self, data):
+    print("")
+    print("[RASPBERRY PI SENSORS]: "+ str(data))
+
+  def stop(self):
+    self._stopped = True
--- pakket.py
+++ pakket.py
@@ -11,11 +11,26 @@ i8 = c_int8; u8 = c_uint8
 i16 = c_int16; u16 = c_uint16
 i32 = c_int32; u32 = c_uint32
 i64 = c_int64; u64 = c_uint64
+_CData = i8.__base__.__base__
+_PyCStructType = ctypes.Structure.__class__
 
-class StructMeta(type):
-    def __init__(self, *args, **kwargs):
-        print(args, kwargs)
-        super().__init__(*args, **kwargs)
 
-class Struct(metaclass = StructMeta):
+class StructMeta(_PyCStructType):
+    def __init__(self, name, bases, d):
+        # print(self, name, bases, d)
+        # _fields_ = []
+        # for annot_name, annot_type in d['__annotations__'].items():
+        #     print(annot_name, annot_type)
+        #     if issubclass(annot_type, _CData):
+        #         _fields_.append((annot_name, annot_type))
+        d['_fields_'] = _fields_
+        # print(_fields_)
+        _fields_ = [('i', i8)]
+        self._fields_ = _fields_
+        super().__init__(name, bases, d)
+
+class Struct(ctypes.Structure, metaclass=StructMeta):
+    # i: i8
     pass
+
+print(Struct().i)
