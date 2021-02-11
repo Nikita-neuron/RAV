4f07625bcf14227cad39734973582b546a9e35a0
Nikita-neuron <077anderwater770@gmail.com>
2021-02-11 17:30:41 +0300

    pakkit(unf)

--- pakkit/types.py
+++ pakkit/types.py
@@ -32,9 +32,9 @@ class DataType(type, metaclass=DataTypeMeta):
     # *args и **kwargs - аргументы для подклассов, они тут как заглушка
     def __new__(DataType, *args, **kwargs):
         # print('DATATYPE NEW', DataType)
-        d = {
+        d = kwargs.get('d', {
             '__pakkit_fromstream__': classmethod(DataType.__pakkit_fromstream__),
-        }
+        })
         for name, value in DataType.__dict__.items():
             if name.startswith('instance_'):
                 d[name[len('instance_'):]] = value
@@ -59,8 +59,10 @@ class SimpleData(DataType):
         self.cdata = self._ctype(value)
     def __pakkit_fromstream__(cls, stream):
         self = cls()
-        self.cdata = self._ctype.from_buffer_copy(stream.read(ctypes.sizeof(self._ctype)))
+        self.cdata = self._ctype.from_buffer_copy(stream.read(sizeof(self._ctype)))
         return self
+    def __instance_pakkit_tostream__(self, stream):
+        stream.write(self.cdata)
     def __repr__(cls):
         return f'{cls.__name__}'
     def __instance_str__(self):
@@ -80,7 +82,7 @@ for name, ctype in dict(
         i64 = c_int64, u64 = c_uint64).items():
     globals()[name] = SimpleData(ctype, name)
 
-size_t = ctypes.c_size_t
+size_t = ctypes.c_uint32
 
 # class SimpleData(Data):
 
@@ -90,17 +92,24 @@ size_t = ctypes.c_size_t
 
 class Array(DataType):
     def __init__(ArrayT, dtype, size=...):
-        print('ARRAY INIT', dtype, size)
         ArrayT.dtype = dtype
-        ArrayT.size = size
+        ArrayT.size = size if size is ... else size_t(size)
     def __pakkit_fromstream__(cls, stream):
         self = cls()
-        # self.size = size_t(stream.read(sizeof(size_t)))
+        if self.size is ...:
+            size = size_t.from_buffer_copy(stream.read(sizeof(size_t)))
+        else:
+            size = self.size
         self.__data = []
-        for _ in range(self.size):
+        for _ in range(size.value):
             self.__data.append(self.dtype.__pakkit_fromstream__(stream))
+        return self
     def __instance_pakkit_tostream__(self, stream):
-        stream.write(size_t(self.size))
+        if self.size is ...:
+            stream.write(size_t(len(self.__data)))
+        for value in self.__data:
+            value.__pakkit_tostream__(stream)
+        
     def instance_as_list(self):
         return self.__data[:]
     def instance_iter(self):
@@ -111,13 +120,49 @@ class Array(DataType):
         s = ', '.join(map(lambda v: str(v), self.as_list()))
         return f'{type(self)}{{{s}}}'
 
+class StructType(DataType):
+    def __init__(Struct, name, bases, d: dict):
+        fields = d.get('__pakkit_fields__', {})
+        print('new', Struct, name, bases, d)
+        if '__annotations__' in d:
+            for name, dtype in d['__annotations__'].items():
+                if issubclass(dtype, Data):
+                    fields[name] = dtype
+        Struct.__pakkit_fields__ = fields
+    def __pakkit_fromstream__(cls, stream):
+        self = cls()
+        for name, dtype in cls.__pakkit_fields__.items():
+            self.__dict__[name] = dtype.__pakkit_fromstream__(stream)
+        return self
+    def __instance_pakkit_tostream__(self, stream):
+        for name in self.__pakkit_fields__:
+            self.__dict__[name].__pakkit_tostream__(stream)
+    def __instance_repr__(self)
+class Struct(metaclass=StructType):
+    pass
+class MyStruct(Struct):
+    num: i32
+    array_static: Array[i8, 4]
+    array_dynamic: Array[i8, ...]
 
 def main():
     import io
-    stream = io.BytesIO(b'\x00\x01\x02\x03')
-    t = Array[u8, 4]
-    arr = t.__pakkit_fromstream__(stream)
-    print(arr)
+    #                        #               #               #
+    stream_in = io.BytesIO(b'\x01\x00\x00\x00\x01\x02\x03\x04\x05\x00\x00\x00\x05\x04\x03\x02\x01')
+    stream_out = io.BytesIO()
+    # MyStruct.__pakkit_fromstream__(stream_in)
+    s = MyStruct.__pakkit_fromstream__(stream_in)
+    s.__pakkit_tostream__(stream_out)
+    print(s.array_dynamic)
+
+    # stream_in = io.BytesIO(b'\x00\x01\x02\x03')
+    # t = Array[u8, 4]
+    # arr = t.__pakkit_fromstream__(stream_in)
+    # print(arr)
+    # stream_out = io.BytesIO()
+    # arr.__pakkit_tostream__(stream_out)
+    # print('v', stream_out.getvalue())
+
 
 if __name__ == '__main__':
     main()
