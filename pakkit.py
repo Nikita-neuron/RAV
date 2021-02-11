62ff5d990e0eedb3fa1b27000927e28be038fd21
Nikita-neuron <077anderwater770@gmail.com>
2021-02-11 09:50:31 +0300

    pakkit(unf)

--- pakkit.py
+++ pakkit.py
@@ -6,8 +6,7 @@ from ctypes import (
     c_int32, c_uint32,
     c_int64, c_uint64,
 )
-import struct
-from typing import Union, Tuple
+
 
 i8 = c_int8; u8 = c_uint8
 i16 = c_int16; u16 = c_uint16
@@ -16,63 +15,41 @@ i64 = c_int64; u64 = c_uint64
 # _CData = i8.__base__.__base__
 # _PyCStructType = ctypes.Structure.__class__
 
+
+class Data(metaclass=):
+
 class DataTypeMeta(type):
-    def __new__(cls, name, bases, d: dict):
-        print('METANEW')
-        return super().__new__(cls, name, bases, d)
     def __getitem__(self, args):
-        print('GETITEM', self, args)
         if type(args) == tuple:
             return self(*args)
         return self(args)
 
 class DataType(type, metaclass=DataTypeMeta):
-    def __new__(cls, *args): # SHOULD BE IMPLEMENTED IN SUBCLASSES 
-        print('DTYPENEW', cls, args, '|', cls.__name__, cls.__bases__, cls.__dict__)
-        #                              #TODO
-        #                     (может можно сделать имя получше)
-        return super().__new__(cls, cls.__name__, (), dict(cls.__dict__))
-    def __init__(self, *args):
-        print('DATATYPE INIT')
-        # super().__init__(self, *args)
+    # *args и **kwargs - аргументы для подклассов, они тут как заглушка
+    def __new__(DataType, *args, **kwargs):
+        print('DATATYPE NEW', DataType)
+        return super().__new__(DataType, f'{DataType.__name__}T', (Data,), {
+            '__pakkit_fromstream__': classmethod(DataType.__pakkit_fromstream__)
+        })
+    def __pakkit_fromstream__(self, stream):
+        raise NotImplementedError()
+
+# class SimpleData(Data):
+
+
+# def instancemethod(f):
+#     return f
 
 class Array(DataType):
-    # def __init__(self, t: PakkitData, length: Union[int, Ellipsis] = ...):
-    #     self.t = t
-    #     self.length = length
-    # def __pakkit_init__(self, dtype: Data, size=...):
-    #     self.dtype = dtype
-    #     self.size = size
-    def __init__(self, *args):
-        print('ARRAYTYPE INIT', args)
+    def __init__(ArrayT, dtype, size=...):
+        print('ARRAY INIT', dtype, size)
+        ArrayT.dtype = dtype
+        ArrayT.size = size
+    def __pakkit_fromstream__(self, stream):
+        print('ARRAY FROMSTREAM', self, stream)
+        return stream
 
-class ArrayInstance(metaclass=Array):
-    def __init__(self, lst):
-        print('ACTUAL INIT', lst)
 
-# class StructMeta(DataMeta):
-#     def __new__(cls, name, bases, d: dict):
-#         print(cls, name, bases, d)
-#         # _pakkit_fields_ = d.get('__pakkit_fields__', [])
-#         # __pakkit_format = ''
-#         # for annot_name, annot_type in d['__annotations__'].items():
-#         #     print(annot_name, annot_type)
-#         #     if issubclass(annot_type, Data):
-#         #         _pakkit_fields_.append((annot_name, annot_type))
-#         super().__new__(cls, name, bases, d)
-#     # def _frombytes
+ArrayType = Array[int, 8]
 
-# class Struct(metaclass=StructMeta):
-#     _pakkit_fields_ = 123
-#     # i: i8
-#     pass
-
-'''
-class MyStruct(Struct):
-    i: i8
-    arr: Array[1,]
-'''
-
-o = ArrayInstance([1, 2, 3])
-print('DONE', type(o))
-
+print(ArrayType.__pakkit_fromstream__('govnostream'))
--- testttt.py
+++ testttt.py
@@ -1,29 +1,38 @@
 
-class DataType(type):
+
+class Data:
     pass
-    # def __new__(Array, *args, **kwargs):
-    #     return super
+
+class DataTypeMeta(type):
+    def __getitem__(self, args):
+        if type(args) == tuple:
+            return self(*args)
+        return self(args)
+
+class DataType(type, metaclass=DataTypeMeta):
+    # *args и **kwargs - аргументы для подклассов, они тут как заглушка
+    def __new__(DataType, *args, **kwargs):
+        print('DATATYPE NEW', DataType)
+        return super().__new__(DataType, f'{DataType.__name__}T', (Data,), {
+            '__pakkit_fromstream__': classmethod(DataType.__pakkit_fromstream__)
+        })
+    def __pakkit_fromstream__(self, stream):
+        raise NotImplementedError()
+
+
+# def instancemethod(f):
+#     return f
 
 class Array(DataType):
-    def __new__(Array, dtype):
-        print('ARRAY NEW', Array, dtype)
-        def wr(stream):
-            return Array.__pakkit_fromstream__(result(), stream)
-        result = super().__new__(Array, 'ArrayT', (), {'__pakkit_fromstream__': classmethod(sel)})
-        
-        return result
+    def __init__(ArrayT, dtype, size=...):
+        print('ARRAY INIT', dtype, size)
+        ArrayT.dtype = dtype
+        ArrayT.size = size
     def __pakkit_fromstream__(self, stream):
         print('ARRAY FROMSTREAM', self, stream)
         return stream
-        # self.data = 
-    # def __call__(self, *args, **kwargs):
-    #     print('ARRAY CALL', args, kwargs)
-    # def __init__(self, dtype):
-    #     # print('ARRAY NEW', Array, name, bases, d)
-    #     print('ARRAY INIT', self, dtype)
-    #     return super().__new__(Array, self.__name__, (), {})
-    pass
 
 
-ArrayType = Array(int)
+ArrayType = Array[int, 8]
+
 print(ArrayType.__pakkit_fromstream__('govnostream'))
