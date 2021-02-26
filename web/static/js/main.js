fdd79f2989597b8e4fc70cb6a22c2332a7a4eeab
Nikita-neuron <64783326+Nikita-neuron@users.noreply.github.com>
2021-02-26 22:03:24 +0300

    add system data to site

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,21 +1,53 @@
-
 var socket = io();
-
-var sensorsMemoryPercent = document.querySelector();
-var sensorsMemoryTotal = document.querySelector(".sensors-memory-total");
-var sensorsMemoryFree = document.querySelector(".sensors-memory-free");
-var sensorsDiskPercent = document.querySelector(".sensors-disk-percent");
 
 let sensorsSystemData = {
     "memory": {
-        "memoryFree": ".sensors-memory-percent",
-        "memoryTotal": ,
-        "memoryPercent":
+        "memoryFree":       1,
+        "memoryTotal":      2,
+        "memoryPercent":    3
     },
     "disk": {
-        "diskFree": ,
-        "diskTotal": ,
-        "diskPercent": 
+        "diskFree":         4,
+        "diskTotal":        5,
+        "diskPercent":      6
+    },
+    "cpu":                  7,
+    "temperature":          8
+};
+
+let motorsSystemData = {
+    "memory": {
+        "memoryFree":       9,
+        "memoryTotal":      10,
+        "memoryPercent":    11
+    },
+    "disk": {
+        "diskFree":         12,
+        "diskTotal":        13,
+        "diskPercent":      14
+    },
+    "cpu":                  15,
+    "temperature":          16
+};
+
+set_system_data({
+    "sensors": sensorsSystemData,
+    "motors": motorsSystemData
+});
+
+function set_system_data(systemData) {
+    for (let device in systemData) {
+      let data = systemData[device];
+
+      for (let key in data) {
+        if (typeof(data[key]) == "object") {
+          for (let k in data[key]) {
+            document.querySelector(`.${device}-${k}`).innerHTML = data[key][k];
+          }        
+        } else {
+          document.querySelector(`.${device}-${key}`).innerHTML = data[key];
+        }
+      }
     }
 }
 
