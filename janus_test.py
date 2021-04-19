485e373b1c715865dba02993125858b3ac220efe
Nikita-neuron <64783326+Nikita-neuron@users.noreply.github.com>
2021-04-19 08:36:29 +0300

    Add files via upload
    janus

---
+++ janus_test.py
@@ -0,0 +1,351 @@
+import argparse
+import asyncio
+import logging
+import random
+import string
+import time
+
+import aiohttp
+
+import cv2
+import numpy as np
+
+from av import VideoFrame
+
+from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack, MediaStreamTrack
+from aiortc.contrib.media import MediaPlayer, MediaRecorder
+
+pcs = set()
+
+
+def transaction_id():
+    return "".join(random.choice(string.ascii_letters) for x in range(12))
+
+
+# python janus_test.py --record-to test1.mp3  http://192.168.1.38:8088/janus
+
+
+class JanusPlugin:
+    def __init__(self, session, url):
+        self._queue = asyncio.Queue()
+        self._session = session
+        self._url = url
+
+    async def send(self, payload):
+        message = {"janus": "message", "transaction": transaction_id()}
+        message.update(payload)
+        async with self._session._http.post(self._url, json=message) as response:
+            data = await response.json()
+            assert data["janus"] == "ack"
+
+        response = await self._queue.get()
+        assert response["transaction"] == message["transaction"]
+        return response
+
+
+class JanusSession:
+    def __init__(self, url):
+        self._http = None
+        self._poll_task = None
+        self._plugins = {}
+        self._root_url = url
+        self._session_url = None
+
+    async def attach(self, plugin_name: str) -> JanusPlugin:
+        message = {
+            "janus": "attach",
+            "plugin": plugin_name,
+            "transaction": transaction_id(),
+        }
+        async with self._http.post(self._session_url, json=message) as response:
+            data = await response.json()
+            assert data["janus"] == "success"
+            plugin_id = data["data"]["id"]
+            plugin = JanusPlugin(self, self._session_url + "/" + str(plugin_id))
+            self._plugins[plugin_id] = plugin
+            return plugin
+
+    async def create(self):
+        self._http = aiohttp.ClientSession()
+        message = {"janus": "create", "transaction": transaction_id()}
+        async with self._http.post(self._root_url, json=message) as response:
+            data = await response.json()
+            assert data["janus"] == "success"
+            session_id = data["data"]["id"]
+            self._session_url = self._root_url + "/" + str(session_id)
+
+        self._poll_task = asyncio.ensure_future(self._poll())
+
+    async def destroy(self):
+        if self._poll_task:
+            self._poll_task.cancel()
+            self._poll_task = None
+
+        if self._session_url:
+            message = {"janus": "destroy", "transaction": transaction_id()}
+            async with self._http.post(self._session_url, json=message) as response:
+                data = await response.json()
+                assert data["janus"] == "success"
+            self._session_url = None
+
+        if self._http:
+            await self._http.close()
+            self._http = None
+
+    async def _poll(self):
+        while True:
+            params = {"maxev": 1, "rid": int(time.time() * 1000)}
+            async with self._http.get(self._session_url, params=params) as response:
+                data = await response.json()
+                if data["janus"] == "event":
+                    plugin = self._plugins.get(data["sender"], None)
+                    if plugin:
+                        await plugin._queue.put(data)
+                    else:
+                        print(data)
+
+
+class VideoTransformTrack(MediaStreamTrack):
+    """
+    A video stream track that transforms frames from an another track.
+    """
+
+    kind = "video"
+
+    def __init__(self, track, transform):
+        super().__init__()  # don't forget this!
+        self.track = track
+        self.transform = transform
+
+    async def recv(self):
+        frame = await self.track.recv()
+
+        if self.transform == "cartoon":
+            img = frame.to_ndarray(format="bgr24")
+
+            # prepare color
+            img_color = cv2.pyrDown(cv2.pyrDown(img))
+            for _ in range(6):
+                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+
+            # prepare edges
+            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img_edges = cv2.adaptiveThreshold(
+                cv2.medianBlur(img_edges, 7),
+                255,
+                cv2.ADAPTIVE_THRESH_MEAN_C,
+                cv2.THRESH_BINARY,
+                9,
+                2,
+            )
+            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+
+            # combine color and edges
+            img = cv2.bitwise_and(img_color, img_edges)
+
+            # rebuild a VideoFrame, preserving timing information
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+            return new_frame
+        elif self.transform == "edges":
+            # perform edge detection
+            img = frame.to_ndarray(format="bgr24")
+            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+            # rebuild a VideoFrame, preserving timing information
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+            return new_frame
+        elif self.transform == "rotate":
+            # rotate image
+            img = frame.to_ndarray(format="bgr24")
+            rows, cols, _ = img.shape
+            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+            img = cv2.warpAffine(img, M, (cols, rows))
+
+            # rebuild a VideoFrame, preserving timing information
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+            return new_frame
+        else:
+            return frame
+
+
+async def publish(plugin, player):
+    """
+    Send video to the room.
+    """
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+
+    # configure media
+    media = {"audio": False, "video": True}
+
+    if player:
+      print("Play")
+      pc.addTrack(player)
+    else:
+        pc.addTrack(VideoStreamTrack())
+
+    # send offer
+    await pc.setLocalDescription(await pc.createOffer())
+    request = {"request": "configure"}
+    request.update(media)
+    response = await plugin.send(
+        {
+            "body": request,
+            "jsep": {
+                "sdp": pc.localDescription.sdp,
+                "trickle": False,
+                "type": pc.localDescription.type,
+            },
+        }
+    )
+
+    # apply answer
+    await pc.setRemoteDescription(
+        RTCSessionDescription(
+            sdp=response["jsep"]["sdp"], type=response["jsep"]["type"]
+        )
+    )
+
+
+async def subscribe(session, room, feed, recorder, plugin_last):
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+
+    frame = None
+
+    local_video = None
+
+    @pc.on("track")
+    async def on_track(track):
+        print("Track %s received" % track.kind)
+        if track.kind == "video":
+
+          print(feed)
+
+          local_video = VideoTransformTrack(
+                track, transform='cartoon'
+          )
+          pc.addTrack(local_video)
+
+          await publish(plugin=plugin_last, player=local_video)
+
+    # subscribe
+    plugin = await session.attach("janus.plugin.videoroom")
+    response = await plugin.send(
+        {"body": {"request": "join", "ptype": "subscriber", "room": room, "feed": feed}}
+    )
+
+    # apply offer
+    await pc.setRemoteDescription(
+        RTCSessionDescription(
+            sdp=response["jsep"]["sdp"], type=response["jsep"]["type"]
+        )
+    )
+
+    # send answer
+    await pc.setLocalDescription(await pc.createAnswer())
+    response = await plugin.send(
+        {
+            "body": {"request": "start"},
+            "jsep": {
+                "sdp": pc.localDescription.sdp,
+                "trickle": False,
+                "type": pc.localDescription.type,
+            },
+        }
+    )
+    # await recorder.start()
+    return local_video
+
+
+async def run(player, recorder, room, session):
+    await session.create()
+
+    # join video room
+    plugin = await session.attach("janus.plugin.videoroom")
+    response = await plugin.send(
+        {
+            "body": {
+                "display": "aiortc",
+                "ptype": "publisher",
+                "request": "join",
+                "room": room,
+            }
+        }
+    )
+    publish = None
+    publishers = response["plugindata"]["data"]["publishers"]
+    for publisher in publishers:
+        print("id: %(id)s, display: %(display)s" % publisher)
+        if publisher["display"] == "test":
+          publish = publisher
+
+    # receive video
+    # if recorder is not None and publishers:
+    if publish is not None:
+      local_video = await subscribe(
+        session=session, room=room, feed=publish["id"], recorder=recorder, plugin_last=plugin
+      )
+
+    # # send video
+    # await publish(plugin=plugin, player=local_video)
+
+    # exchange media for 10 minutes
+    print("Exchanging media")
+    await asyncio.sleep(600)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Janus")
+    parser.add_argument("url", help="Janus root URL, e.g. http://localhost:8088/janus")
+    parser.add_argument(
+        "--room",
+        type=int,
+        default=1234,
+        help="The video room ID to join (default: 1234).",
+    ),
+    parser.add_argument("--play-from", help="Read the media from a file and sent it."),
+    parser.add_argument("--record-to", help="Write received media to a file."),
+    parser.add_argument("--verbose", "-v", action="count")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    # create signaling and peer connection
+    session = JanusSession(args.url)
+
+    # create media source
+    if args.play_from:
+        player = MediaPlayer(args.play_from)
+    else:
+        player = None
+
+    # create media sink
+    if args.record_to:
+        recorder = MediaRecorder(args.record_to)
+    else:
+        recorder = None
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            run(player=player, recorder=recorder, room=args.room, session=session)
+        )
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if recorder is not None:
+            loop.run_until_complete(recorder.stop())
+        loop.run_until_complete(session.destroy())
+
+        # close peer connections
+        coros = [pc.close() for pc in pcs]
+        loop.run_until_complete(asyncio.gather(*coros))
